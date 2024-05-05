cd6bcb555d419552f9141891985b6afc6067b3c0
robgit21 <79367605+robgit21@users.noreply.github.com>
2024-05-05 21:36:13 +0200

    init

---
+++ components/NavigationComp.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NavigationComp() {
+  return <div>Nav</div>;
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import LayoutComp from "../../components/LayoutComp";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <LayoutComp>
+      <Component {...pageProps} />
+    </LayoutComp>
+  );
 }
