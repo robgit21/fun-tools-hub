56126de770706e487c3d38da96ec08ac71180acc
robgit21 <79367605+robgit21@users.noreply.github.com>
2024-05-05 22:06:47 +0200

    update speedinsights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <LayoutComp>
+      <SpeedInsights />
       <Component {...pageProps} />
     </LayoutComp>
   );
