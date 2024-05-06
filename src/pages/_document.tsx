f180af97bb9e4acef4664a0400a962afc66e55f6
robgit21 <79367605+robgit21@users.noreply.github.com>
2024-05-06 22:21:51 +0200

    remove ads

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,20 +5,6 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body className="mainbackground">
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7501384629405523"
-          crossOrigin="anonymous"
-        ></script>
-        <ins
-          className="adsbygoogle"
-          style={{ display: "block" }}
-          data-ad-client="ca-pub-7501384629405523"
-          data-ad-slot="1793656643"
-          data-ad-format="auto"
-          data-full-width-responsive="true"
-        ></ins>
-        <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
         <Main />
         <NextScript />
       </body>
